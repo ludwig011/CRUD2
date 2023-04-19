21ad2d95d34140666ef6dbf07b4c02ade327f380
ludgwig011 <lopez.metalurgico@gmail.com>
2023-04-19 18:22:35 -0400

    ya

--- index.php
+++ index.php
@@ -374,25 +374,25 @@
 <script>
     var modal = document.getElementById("myModal");
     var modal2 = document.getElementById("myModal2");
-    
-    $(".btn-danger").on("click", function() {
+
+    $(".btn-danger").on("click", function () {
         modal2.style.display = "block";
         // console.log(this.parentElement.parentElement);
-        var currentRow=$(this).closest("tr");
-        
+        var currentRow = $(this).closest("tr");
+
         const pieza = currentRow.find("td:eq(0)").text().replace(/\s/g, "");
         const colada = currentRow.find("td:eq(1)").text().replace(/\s/g, "");
         const dueno = currentRow.find("td:eq(2)").text().replace(/\s/g, "");
         const almas = currentRow.find("td:eq(3)").text().replace(/\s/g, "");
-        const almas_detalle = currentRow.find("td:eq(5)").text().replace(/\s/g, "");
-        
+        const moldeo = currentRow.find("td:eq(3)").text().replace(/\s/g, "");
+
         document.getElementById("pieza").value = pieza;
         document.getElementById("colada").value = colada;
         document.getElementById("dueno").value = dueno;
         document.getElementById("almas").value = almas;
-
+        document.getElementById("moldeo").value = moldeo;
     });
-    
+
     var btn = document.getElementById("myBtn");
 
     var span = document.getElementsByClassName("close")[0];
