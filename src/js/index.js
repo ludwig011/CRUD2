62d6b2187a12583b4f5dab2884d661ebecec2907
Rodrigo Reyes <rod.reyes.s@gmail.com>
2023-04-24 12:27:44 -0400

    primeros datos cargados en index.html

--- src/js/index.js
+++ src/js/index.js
@@ -48,9 +48,42 @@ $(document).ready(function () {
     const xhttp = new XMLHttpRequest();
 
     xhttp.onload = function () {
-        const response = JSON.parse(this.response);
-        console.log(response);
-        console.log(typeof response);
+        const array = JSON.parse(this.response);
+
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            const pieza = element['pieza'];
+            const colada = element['colada'];
+            const dueno = element['dueno'];
+
+            console.log(element);
+
+            const card = '<div class="card">' +
+                '<table class="table-sm table-hover table-striped table-dark">' +
+                '<tbody>' +
+
+                '<tr>' +
+                '<th class="header tg" scope="col">Pieza</th>' +
+                '<th class="header tg" scope="col">Colada</th>' +
+                '<th class="header tg" scope="col">Dueño</th>' +
+                '<th class="header tg" scope="col">Área</th>' +
+                '<th class="header tg" scope="col">Detalle</th>' +
+                '<th class="header tg" scope="col">Acción</th>' +
+                '</tr>' +
+
+                '<tr>' +
+                '<td class="tg" rowspan="8">' + pieza + '</td>' +
+                '<td class="tg" rowspan="8">' + colada + '</td>' +
+                '<td class="tg" rowspan="8">' + dueno + '</td>' +
+                '<td>ALMAS</td><td></td><td class="tg" rowspan="8">' +
+                '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#staticBackdrop">Modificar</button><a href="eliminar.php?id=" class="btn btn-warning" onclick="return confirmar()"><i class="fa-solid fa-trash-can"></i>ELIMINAR</a></td></tr><tr><td>MOLDEO</td><td></td></tr><tr><td>FUSION</td><td></td></tr><tr><td>FLOGGY</td><td></td></tr><tr><td>T.T.</td><td></td></tr><tr><td>INSPECCION</td><td></td></tr><tr><td>FINISHING</td><td></td></tr><tr><td>SCANNER</td><td></td></tr></tbody></table></div>';
+
+            const cardContainer = document.getElementById("cardContainer");
+            cardContainer.innerHTML += (card);
+
+        }
+
+
     }
 
     xhttp.open("GET", "http://localhost/crud2/src/php/getAllZonas.php", true);
