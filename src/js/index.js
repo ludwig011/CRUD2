bd00811fe1af73509ae3382f4d13d0361ee792f9
Rodrigo Reyes <rod.reyes.s@gmail.com>
2023-04-24 12:41:24 -0400

    agrego resto de datos a index.html

--- src/js/index.js
+++ src/js/index.js
@@ -51,17 +51,25 @@ $(document).ready(function () {
         const array = JSON.parse(this.response);
 
         for (let index = 0; index < array.length; index++) {
+
             const element = array[index];
             const pieza = element['pieza'];
             const colada = element['colada'];
             const dueno = element['dueno'];
-
-            console.log(element);
+            const almas = element['almas'];
+            const moldeo = element['moldeo'];
+            const fusion = element['fusion'];
+            const floggy = element['floggy'];
+            const tratamientos = element['tratamientos'];
+            const inspeccion = element['inspeccion'];
+            const finishing = element['finishing'];
+            const scanner = "lorem ipsum";
 
             const card = '<div class="card">' +
                 '<table class="table-sm table-hover table-striped table-dark">' +
                 '<tbody>' +
 
+                // table row 1
                 '<tr>' +
                 '<th class="header tg" scope="col">Pieza</th>' +
                 '<th class="header tg" scope="col">Colada</th>' +
@@ -71,12 +79,38 @@ $(document).ready(function () {
                 '<th class="header tg" scope="col">Acción</th>' +
                 '</tr>' +
 
+                // table row 2
                 '<tr>' +
                 '<td class="tg" rowspan="8">' + pieza + '</td>' +
                 '<td class="tg" rowspan="8">' + colada + '</td>' +
                 '<td class="tg" rowspan="8">' + dueno + '</td>' +
-                '<td>ALMAS</td><td></td><td class="tg" rowspan="8">' +
-                '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#staticBackdrop">Modificar</button><a href="eliminar.php?id=" class="btn btn-warning" onclick="return confirmar()"><i class="fa-solid fa-trash-can"></i>ELIMINAR</a></td></tr><tr><td>MOLDEO</td><td></td></tr><tr><td>FUSION</td><td></td></tr><tr><td>FLOGGY</td><td></td></tr><tr><td>T.T.</td><td></td></tr><tr><td>INSPECCION</td><td></td></tr><tr><td>FINISHING</td><td></td></tr><tr><td>SCANNER</td><td></td></tr></tbody></table></div>';
+                '<td>ALMAS</td><td>' + almas + '</td>' +
+
+                //Botones modificar y eliminar
+                '<td class="tg" rowspan="8">' +
+                '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#staticBackdrop">Modificar</button>' +
+                '<a href="eliminar.php?id=" class="btn btn-warning" onclick="return confirmar()"><i class="fa-solid fa-trash-can"></i>ELIMINAR</a>' +
+                '</td>' +
+                '</tr>' +
+
+                // table row 3
+                '<tr><td>MOLDEO</td><td>' + moldeo + '</td></tr>' +
+                // table row
+                '<tr><td>FUSION</td><td>' + fusion + '</td></tr>' +
+                // table row
+                '<tr><td>FLOGGY</td><td>' + floggy + '</td></tr>' +
+                // table row
+                '<tr><td>T.T.</td><td>' + tratamientos + '</td></tr>' +
+                // table row
+                '<tr><td>INSPECCION</td><td>' + inspeccion + '</td></tr>' +
+                // table row
+                '<tr><td>FINISHING</td><td>' + finishing + '</td></tr>' +
+                // table row
+                '<tr><td>SCANNER</td><td>' + scanner + '</td></tr>' +
+
+                '</tbody>' +
+                '</table>' +
+                '</div>';
 
             const cardContainer = document.getElementById("cardContainer");
             cardContainer.innerHTML += (card);
